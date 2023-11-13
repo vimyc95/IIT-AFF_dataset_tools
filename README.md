3b6869afa9fe8be83aec83484db428268f81a71f
vimyc95 <vimyc95.gmail.com>
2023-11-13 13:38:27 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# IIT-AFF_dataset_tools
+## IIT-AFF dataset tools
+### This tool is use to separate away ROI image and object Affordance label from origin image, we can download proccesesed datasets by origin author on [github](https://github.com/nqanh/affordance-net) or [One Drive](https://studenthcmusedu-my.sharepoint.com/:u:/g/personal/nqanh_mso_hcmus_edu_vn/EXQok71Y2kFAmhaabY2TQO8BFIO1AqqH5GcMOfPqgn_q2g?e=7rH3Kd)
+
+### Run
--- deal_v0.2.py
+++ deal_v0.2.py
@@ -36,7 +36,10 @@ for img_n in tqdm.tqdm(txt):
                     # print("{}/{}".format(img_n, afford_n))
 
                     cv2.imwrite('{}/{}_{}.jpg'.format(new_img_dir, img_n, afford_n), img[bbox[1]:bbox[3], bbox[0]:bbox[2],:])
-                    cv2.imwrite('{}/{}_{}.png'.format(new_aff_dir, img_n, afford_n), aff[bbox[1]:bbox[3], bbox[0]:bbox[2]])
+                    
+                    aff_3 = cv2.merge((aff[bbox[1]:bbox[3], bbox[0]:bbox[2]],aff[bbox[1]:bbox[3], bbox[0]:bbox[2]],aff[bbox[1]:bbox[3], bbox[0]:bbox[2]]))
+                    cv2.imwrite('{}/{}_{}.png'.format(new_aff_dir, img_n, afford_n), aff_3)
+                    print(aff_3.shape)
 
                     new_txt.write('{}_{}\n'.format(img_n, afford_n))
                     # cv2.imshow("aff", aff[bbox[1]:bbox[3], bbox[0]:bbox[2]]*50)
